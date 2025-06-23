8277736f6699d7b173df9b811d335b4e227d4f0a
aleksei <aleksei@example.com>
2025-06-23 21:53:03 +0300

    update23_06

--- src/components/Pump.jsx
+++ src/components/Pump.jsx
@@ -1,8 +1,29 @@
 import { SWAP_FROM_322_TO_B_LINE_PROBLEM } from "../App";
 import parkStore from "../store/parkStore";
+const SWAP_FROM_322_TO_GPN_B = [
+  "E-327",
+  "E-328",
+  "E-329",
+  "E-330",
+  "E-331",
+  "E-332",
+  "E-333",
+  "E-334",
+];
 
+const SWAP_115_NOT_INCLUDE = [
+  "Н-1",
+  "Н-2",
+  "Н-4",
+  "Н-5",
+  "H-1",
+  "H-2",
+  "H-3",
+  "H-4",
+];
 const Pump = ({ top = 0, left = 0, title = "" }) => {
-  const { activePump, setActivePump, from, to, lineAorB } = parkStore();
+  const { activePump, setActivePump, from, to, lineAorB, activePumpStation } =
+    parkStore();
   const isActive = activePump.includes(title);
   let colorActivePump =
     "linear-gradient(90deg, green,green,green, #1e22e0,#1e22e0,#1e22e0)";
@@ -12,6 +33,23 @@ const Pump = ({ top = 0, left = 0, title = "" }) => {
   }
 
   const handleClick = () => {
+    if (
+      from === "E-322" &&
+      SWAP_FROM_322_TO_GPN_B.includes(to) &&
+      lineAorB === "Б" &&
+      (title === "H-3" || title === "H-4")
+    ) {
+      return;
+    }
+
+    if (
+      lineAorB.length === 1 &&
+      SWAP_115_NOT_INCLUDE.includes(title) &&
+      activePumpStation === "910-115"
+    ) {
+      return;
+    }
+
     if (
       SWAP_FROM_322_TO_B_LINE_PROBLEM.includes(to) &&
       (title === "H-2" || title === "H-3")
